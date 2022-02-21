8a01fe5318fe4ae4034933dac621d99ef1df8127
Eoin Lynch <sterlynel@gmail.com>
2022-02-21 21:39:12 +0000

    Made progress on day 1

--- day1/src/com/advent/day1/Control.java
+++ day1/src/com/advent/day1/Control.java
@@ -4,7 +4,10 @@ public class Control {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		FileReader fileReader = new FileReader("input.txt");
+		fileReader.openFile();
+		String line = fileReader.readLine();
+		
 	}
 
 }
--- day1/src/com/advent/day1/Counter.java
+++
@@ -1,5 +0,0 @@
-package com.advent.day1;
-
-public class Counter {
-
-}
